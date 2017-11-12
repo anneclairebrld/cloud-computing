b808bf6156039e0e5d89f0aeafc49e187daf4081
Orsi Lukacs <ol14474@my.bristol.ac.uk>
2017-11-12 20:32:28 +0000

    Minor changes in layout

--- src/js/photomodif.js
+++ src/js/photomodif.js
@@ -1,7 +1,6 @@
-
 //    var imageFile = document.getElementById('myImage').innerHTML;
-$(document).ready(function(){
-    
+$(document).ready(function() {
+
     // (C) Ken Fyrstenberg, Epistemex, License: CC3.0-attr
     var ctx = canvas.getContext('2d'),
         img = new Image(),
@@ -61,7 +60,48 @@ $(document).ready(function(){
             }
         }*/
         //i = 0;
-        
+
+    }
+
+    function difficulty(lev) {
+
+        // find which button was pressed
+        if (lev == "e") {
+//            size = 0.06
+            size = 0.01
+        } else if (lev == "m") {
+            size = 0.15
+        } else {
+            size = 0.25
+        }
+
+        // cache scaled width and height
+        w = canvas.width * size,
+        h = canvas.height * size;
+
+        // draw original image to the scaled size
+        ctx.drawImage(img, 0, 0, w, h);
+        //console.log("size : " + size);
+        //console.log(w);
+        //console.log(h);
+
+        // then draw that scaled image thumb back to fill canvas
+        // As smoothing is off the result will be pixelated
+
+        //Orsi, this is where the pixel rgb's are calculated, you can console out and see
+        ctx.drawImage(canvas, 0, 0, w, h, 0, 0, canvas.width, canvas.height);
+//        var i = 1;
+//        for (var x1 = 0; x1<w*1/size; x1+=1/size){
+//            for(var y1 = 0; y1<h*1/size; y1+=1/size){
+//                var r = ctx.getImageData(x1,y1,x1+1/size,y1+1/size).data[0];
+//                var g = ctx.getImageData(x1,y1,x1+1/size,y1+1/size).data[1];
+//                var b = ctx.getImageData(x1,y1,x1+1/size,y1+1/size).data[2];
+//                console.log("rgb " + i + " " + r + " " + g + " " + b);
+//                i++;
+//            }
+//        }
+//        i = 0;
+
     }
 
     // This runs the demo animation to give an impression of
@@ -99,13 +139,17 @@ $(document).ready(function(){
     }
 
     // event listeneners for slider and button
-    blocks.addEventListener('change', pixelate, false);
-    animate.addEventListener('click', toggleAnim, false);
+//    blocks.addEventListener('change', pixelate, false);
+//    animate.addEventListener('click', toggleAnim, false);
+
+    easy.addEventListener('click', function() { difficulty("e") }, false);
+    medium.addEventListener('click', function() { difficulty("m") }, false);
+    hard.addEventListener('click', function() { difficulty("h") }, false);
 
     // poly-fill for requestAnmationFrame with fallback for older
     // browsers which do not support rAF.
-    window.requestAnimationFrame = (function () {
-        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
+    window.requestAnimationFrame = (function() {
+        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function(callback) {
             window.setTimeout(callback, 1000 / 60);
         };
     })();
