16813e2010236e5bdd61357c3953c8eedd64e146
TuanaCelik <tc13007@my.bristol.ac.uk>
2017-11-16 14:24:29 +0000

    the printed out lines on the server are the retrieved image info

--- gs-serving-web-content-master/complete/src/main/java/app/TapController.java
+++ gs-serving-web-content-master/complete/src/main/java/app/TapController.java
@@ -22,8 +22,10 @@ public class TapController {
 
     @RequestMapping(value = "images", method = RequestMethod.POST)
     public @ResponseBody Response saveImage(@RequestBody Picture image64) {
-        System.out.println(image64.getImageData());
         System.out.println("Got a post request to the server");
+        System.out.println("base64 String: " + image64.getImageData());
+        System.out.println("Image Width: " + image64.getWidth());
+        System.out.println("Image Height: " + image64.getHeight());
         //byte[] data = Base64.decodeBase64(image64);
         //String s = new String(data);
         //System.out.println(s);
