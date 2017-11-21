553d0f9311da126a7e68f750ee210aa259b10037
Anne-C Bourland <ab14188@my.bristol.ac.uk>
2017-11-21 17:05:34 +0000

    testing sql data base should work

--- gs-serving-web-content-master/complete/src/main/java/app/MySQLConnection.java
+++ gs-serving-web-content-master/complete/src/main/java/app/MySQLConnection.java
@@ -1,105 +1,82 @@
 package app;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MySQLConnection {
-    private Connection connection = null;
-    private Statement statement = null;
-    private ResultSet resultSet = null;
-    private String instanceConnectionName = null;
-    private String username = null;
-    private String password= null;
-    private String databaseName = null;
+    private final String jdbcDriverStr;
+    private final String jdbcURL;
 
-    public MySQLConnection(){
-        instanceConnectionName = "tap-estry-186513:europe-west1:back-end";
-        username = "annie_test";
-        password = "deletefile69";
-        databaseName =  "images";
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
+    private PreparedStatement preparedStatement;
+
+    private String username= "annie_test";
+    private String password= "deletefile69";
+    enum TestTableColumns{
+        id,TEXT;
     }
 
-    public void connectToDataBase() throws Exception, SQLException{
-        System.out.println("got to the connectToDataBase function");
-        String jdbcUrl = String.format(
-                "jdbc:mysql://google/%s?cloudSqlInstance=%s&"
-                        + "socketFactory=com.google.cloud.sql.mysql.SocketFactory",
-                databaseName,
-                instanceConnectionName);
-        System.out.println("created a jdbcUrl");
-        connection = DriverManager.getConnection(jdbcUrl, username, password);
+    public MySQLConnection(String jdbcDriverStr, String jdbcURL){
+        this.jdbcDriverStr = jdbcDriverStr;
+        this.jdbcURL = jdbcURL;
+    }
 
-        System.out.println("blehhh to database");
-        try (Statement statement = connection.createStatement()) {
-            System.out.println("tconnected to database");
-            ResultSet resultSet = statement.executeQuery("SHOW TABLES");
-            if (resultSet.equals(null) ){
-                System.out.println("there is nothhing in the database");
-            }
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString(1));
-            }
-        }catch (Exception e){
-            throw e;
+    public void readData() throws Exception {
+        try {
+            Class.forName(jdbcDriverStr);
+            connection = DriverManager.getConnection(jdbcURL, username, password);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select * from javaTestDB.test_table;");
+            getResultSet(resultSet);
+            preparedStatement = connection.prepareStatement("insert into javaTestDB.test_table values (default,?)");
+            preparedStatement.setString(1,"insert test from java");
+            preparedStatement.executeUpdate();
+        }finally{
+            close();
+        }
+    }
+    private void getResultSet(ResultSet resultSet) throws Exception {
+        while(resultSet.next()){
+            Integer id = resultSet.getInt(TestTableColumns.id.toString());
+            String text = resultSet.getString(TestTableColumns.TEXT.toString());
+            System.out.println("id: "+id);
+            System.out.println("text: "+text);
         }
     }
 
-    public void readDatabase()  {
-//        try{
-//            // This will load the MySQL driver, each DB has its own driver
-//            Class.forName("com.mysql.jdbc.Driver");
-//
-//            // Setup the connection with the DB -- db calles images
-//            //change the path to connect to the db on the cloud
-//            connection = DriverManager
-//                    .getConnection("jdbc:mysql://localhost/images?"
-//                            + "user=sqluser&password=sqluserpw");
-//
-//            // Statements allow to issue SQL queries to the database
-//            statement = connection.createStatement();
-//            // Result set get the result of the SQL query
-//            resultSet = statement
-//                    .executeQuery("select * from feedback.comments");
-//            writeResultSet(resultSet);
-//
-//            // PreparedStatements can use variables and are more efficient
-//            preparedStatement = connection
-//                    .prepareStatement("insert into  feedback.comments values (default, ?)");
-//            // "imageLocation");
-//            // Parameters start with 1
-//            preparedStatement.setString(1, "ImageLocation");
-//            preparedStatement.executeUpdate();
-//
-//            preparedStatement = connection
-//                    .prepareStatement("SELECT ImageLocation, COMMENTS from feedback.comments");
-//            resultSet = preparedStatement.executeQuery();
-//            writeResultSet(resultSet);
-//
-//            // Remove again the insert comment
-//            preparedStatement = connection
-//                    .prepareStatement("delete from feedback.comments where myuser= ? ; ");
-//            preparedStatement.setString(1, "ImageLocation");
-//            preparedStatement.executeUpdate();
-//
-//            resultSet = statement
-//                    .executeQuery("select * from feedback.comments");
-//            writeMetaData(resultSet);
-//
-//        } catch (Exception e) {
-//            throw e;
-//        } finally {
-//            close();
-//        }
+    private void close(){
+        try {
+            if(resultSet!=null) resultSet.close();
+            if(statement!=null) statement.close();
+            if(connection!=null) connection.close();
+        } catch(Exception e){}
     }
-
-//    public void getImage(String imageId) throws Exception{
-//        try{
-//            //get the precise image asked
+//    public void connectToDataBase() throws Exception{
+//
+//        System.out.println("got to the connectToDataBase function");
+//        String jdbcUrl = String.format(
+//                "jdbc:mysql://google/%s?cloudSqlInstance=%s&"
+//                        + "socketFactory=com.google.cloud.sql.mysql.SocketFactory",
+//                databaseName,
+//                instanceConnectionName);
+//        System.out.println("created a jdbcUrl");
+//        connection = DriverManager.getConnection(jdbcUrl, username, password);
+//
+//        System.out.println("blehhh to database");
+//        try (Statement statement = connection.createStatement()) {
+//            System.out.println("tconnected to database");
+//            ResultSet resultSet = statement.executeQuery("SHOW TABLES");
+//            if (resultSet.equals(null) ){
+//                System.out.println("there is nothhing in the database");
+//            }
+//            while (resultSet.next()) {
+//                System.out.println(resultSet.getString(1));
+//            }
 //        }catch (Exception e){
 //            throw e;
 //        }
 //    }
 
 }
+
+
